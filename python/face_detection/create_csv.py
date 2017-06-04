f7472a02a337218c6c3ab62f37a53a2589325b67
Koisell <nicolas.francois@insa-rouen.fr>
2017-06-04 16:55:07 +0200

    update create csv to only match images

--- python/face_detection/create_csv.py
+++ python/face_detection/create_csv.py
@@ -4,7 +4,7 @@
 
 import sys
 import os.path
-
+from re import match
 # This is a tiny script to help you creating a CSV file from a face
 # database with a similar hierarchie:
 #
@@ -39,7 +39,10 @@ if __name__ == "__main__":
     for dirname, dirnames, filenames in os.walk(BASE_PATH):
         for subdirname in dirnames:
             subject_path = os.path.join(dirname, subdirname)
+            number = match("s(\d+).*", subdirname)
+            label = int(number.group(1))
             for filename in os.listdir(subject_path):
-                abs_path = "%s/%s" % (os.path.abspath(subject_path), filename)
-                print("%s%s%d" % (abs_path, SEPARATOR, label))
+                if match(".*\.(?:png|jpg|pgm|jpeg)", filename.lower()) is not None:
+                    abs_path = "%s/%s" % (os.path.abspath(subject_path), filename)
+                    print("%s%s%d" % (abs_path, SEPARATOR, label))
             label += 1

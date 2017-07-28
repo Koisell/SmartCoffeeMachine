a584f24200985687a50eb68330064cf27d6fc75c
Koisell <nicolas.francois@insa-rouen.fr>
2017-07-29 00:08:17 +0200

    remove legacy website. Still available on legacy_website branch

--- ihm_web/includes/fin.php
+++
@@ -1,6 +0,0 @@
-<?php 
-
-include("bas.php"); // Code HTML
-include("fin_fonctions.php"); // Déconnexion MySQL, fermeture fichier Log, etc.
-
-?>
--- ihm_web/utilisateur/editer_informations.php
+++
@@ -1,4 +0,0 @@
-<?php
-$mise_a_jour_compte = true;
-include("../connexion/inscription.php");
-?>

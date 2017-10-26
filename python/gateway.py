44946df233647085fe19da2d630c2fac3f760fa2
Romain Jacquier <romain.jacquier@insa-rouen.fr>
2017-10-26 16:17:41 +0200

    Add structure for new Gateway module

---
+++ python/gateway.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+# -*- encoding: UTF-8 -*-
+
+from os import getenv
+from socket import gethostname
+from flask import Flask
+from gateway_api import add_route
+
+
+def main():
+    app = Flask(__name__)
+    add_route(app)
+
+    port = getenv('PORT', '5000')
+    app.run(host=gethostname(), port=int(port))
+
+
+if __name__ == '__main__':
+    main()
---
+++ python/gateway_api/flask_app.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python3
+# -*- encoding: UTF-8 -*-
+
+from flask import jsonify, request
+
+def add_route(app):
+    ''' Add routes to a flask app Class. See API swagger doc'''
+    @app.route('/users/<id>', methods=['GET'])
+    def get_user(id):
+        try:
+            id = int(id)
+        except ValueError:
+            return "Invalid id", 400
+        print(id)
+        # TO BE IMPLEMENTED
+        # ASK RECOGNITION SERVICE FOR USER(id)
+        if result:
+            return jsonify(dict(result.items()))
+        else:
+            return "User not found", 404
+
+    @app.route('/users', methods=["POST"])
+    def new_user():
+        body = request.get_json()
+        try:
+            username, intensity, volume = [body[k] for k in ("username", "intensity", "volume")]
+        except KeyError:
+            return "Incorrect body", 400
+        # TO BE IMPLEMENTED
+        # ASK RECOGNITION SERVICE TO CREATE USER
+        return jsonify(dict(result.items()))
+
+    @app.route('/users/<id>', methods=["PUT"])
+    def modify_user(id):
+        try:
+            id = int(id)
+        except ValueError:
+            return "Invalid id", 400
+        # TO BE IMPLEMENTED
+        # ASK RECOGNITION SERVICE TO MODIY USER
+        #     return jsonify(dict(result.items()))
+        # else:
+            return "User not found", 404
+
+    @app.route('/users/<id>', methods=["DELETE"])
+    def delete_user(id):
+        try:
+            id = int(id)
+        except ValueError:
+            return "Invalid id", 400
+
+        # TO BE IMPLEMENTED
+        # ASK RECOGNITION SERVICE TO DELETE USER
+        # if find
+        #     return "User succesfuly destroyed"
+        # else:
+            return "User not found", 404
+
+    @app.route('/coffee', methods=["GET"])
+    def make_coffee():
+        print(request.values)
+        # TO BE IMPLEMENTED
+        # ASK COFFE MACHINE TO MAKE COFFE
+        return "", 200

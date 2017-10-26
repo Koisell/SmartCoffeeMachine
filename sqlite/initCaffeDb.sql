9cfed49f1ba9d547dcd126bdfdf962efc85f5628
Romain Jacquier <romain.jacquier@insa-rouen.fr>
2017-10-26 16:16:25 +0200

    add inition script for sqlite DB

---
+++ sqlite/initCaffeDb.sql
@@ -0,0 +1,2 @@
+CREATE TABLE if not exists User (id INTEGER PRIMARY KEY AUTOINCREMENT, username TEXT,intensity INTEGER, volume INTEGER);
+INSERT INTO User (username,intensity,volume) VALUES ('Romain','3','3');

00a9c6c5154b5c0ab019205d780918e2ff220478
Koisell <koisell.dev@gmail.com>
2017-11-01 16:18:24 +0100

    send doc

--- python/gateway_api/__init__.py
+++ python/gateway_api/__init__.py
@@ -1,1 +1,2 @@
 from .flask_app import add_route
+from .doc_sender import add_doc
---
+++ python/gateway_api/doc_sender.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# coding: utf-8
+
+from flask import send_from_directory
+
+
+def add_doc(app, path_to_doc):
+    @app.route("/doc/coffee_machine")
+    def send_coffee_machine_doc():
+        return send_from_directory(path_to_doc, "CoffeeService.yaml", as_attachment=True)
+
+    @app.route("/doc/gateway")
+    def send_gateway_doc():
+        return send_from_directory(path_to_doc, "Gateway.yaml", as_attachment=True)
